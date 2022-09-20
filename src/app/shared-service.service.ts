962190e8f7245a390b961e80287f9970be27b5e9
Anushka Ghosh <anushka.ghosh.Contractor@pepsico.com>
2022-09-20 22:17:59 +0530

    dist

--- src/app/shared-service.service.ts
+++ src/app/shared-service.service.ts
@@ -9,60 +9,60 @@ export class SharedServiceService {
 
   constructor(private http:HttpClient) { }
 
-  baseUrl : String = environment.baseUrl;
+  // baseUrl : String = environment.baseUrl;
 
   registerUser(user:any):Observable<any>{
-    return this.http.post<any>(this.baseUrl+'/api/v1.0/tweets/register',user);
+    return this.http.post<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/register',user);
   }
 
   login(loginUser:any):Observable<any>{
-    return this.http.post<any>(this.baseUrl+'/api/v1.0/tweets/login',loginUser);
+    return this.http.post<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/login',loginUser);
   }
 
   getUsers():Observable<any>{
-    return this.http.get<any>(this.baseUrl+'/api/v1.0/tweets/users/all');
+    return this.http.get<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/users/all');
   }
 
   searchUser(userName:any):Observable<any>{
-    return this.http.get<any>(this.baseUrl+'/api/v1.0/tweets/user/search/'+userName);
+    return this.http.get<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/user/search/'+userName);
   }  
 
   forgotPassword(userName:any,password:any):Observable<any>{
-    return this.http.put<any>(this.baseUrl+'/api/v1.0/tweets/'+userName+'/forgot',password);
+    return this.http.put<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+userName+'/forgot',password);
   }
 
   addTweet(userName:any,tweet:any):Observable<any>{
-    return this.http.post<any>(this.baseUrl+'/api/v1.0/tweets/'+userName+'/add',tweet,{
+    return this.http.post<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+userName+'/add',tweet,{
       observe:'body',responseType:'text' as 'json'
     });
   }
 
   getTweets():Observable<any>{
-    return this.http.get<any>(this.baseUrl+'/api/v1.0/tweets/all');
+    return this.http.get<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/all');
   }
 
   viewTweets(userName:any):Observable<any>{
-    return this.http.get<any>(this.baseUrl+'/api/v1.0/tweets/'+userName);
+    return this.http.get<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+userName);
   }
 
   deleteTweet(userName:any,id:any):Observable<any>{
-    return this.http.delete(this.baseUrl+'/api/v1.0/tweets/'+userName+'/delete/'+id,{
+    return this.http.delete('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+userName+'/delete/'+id,{
       observe:'body',responseType:'text' as 'json'
     });
   }
 
   updateTweet(userName:any,id:any,tweet:any):Observable<any>{
-    return this.http.put<any>(this.baseUrl+'/api/v1.0/tweets/'+userName+'/update/'+id,tweet);
+    return this.http.put<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+userName+'/update/'+id,tweet);
   }
 
   addLike(userName:any,id:any):Observable<any>{
-    return this.http.put<any>(this.baseUrl+'/api/v1.0/tweets/'+userName+'/like/'+id,{
+    return this.http.put<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+userName+'/like/'+id,{
       observe:'body',responseType:'text' as 'json'
     });
   }
 
   addReply(username:any,id:any,reply:any):Observable<any>{
-    return this.http.post<any>(this.baseUrl+'/api/v1.0/tweets/'+username+'/reply/'+id,reply,{
+    return this.http.post<any>('https://backend-tweet.azurewebsites.net/api/v1.0/tweets/'+username+'/reply/'+id,reply,{
       observe:'body',responseType:'text' as 'json'
     });
   }
